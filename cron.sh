ce3949e807219aed6f16e41af31891c7cc2090e8
root <root@localhost.localdomain>
2015-12-29 13:42:02 +0800

    update cron job script to add db container.

--- cron.sh
+++ cron.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-#delete all the containers
+echo "1. Delete all the containers"
 docker rm -f $(docker ps -a -q)
 
-#delete untagged images
+echo "2. Delete untagged images"
 docker rmi -f $(docker images --filter dangling=true -q)
 
 
-#pull the latest image
+echo "3. Pull the latest images"
 docker pull grayzone/screening
 docker pull grayzone/postgresql
 
-# launch
-docker-compose up -d
+echo "4. Create db container."
+docker run --name postgresql -d -v /var/lib/pgsql/data:/var/lib/pgsql/data grayzone/postgresql
+
+echo "sleep 5 seconds to wait for db service is ready."
+sleep 5
+
+echo "5. Create web container."
+docker run --name screening --link postgresql -d -p 80:8080 grayzone/screening
 
 
 
